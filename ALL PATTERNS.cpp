eeae15d8253464283a0dd9c544f309be33248faf
Prathamesh Jadhav <109234076+pratham0709@users.noreply.github.com>
2023-02-22 20:41:02 +0530

    Update ALL PATTERNS.cpp

--- ALL PATTERNS.cpp
+++ ALL PATTERNS.cpp
@@ -528,3 +528,27 @@ void getStarPattern(int n) {
             cout<<endl;
         }
 }
+
+Pattern 22
+4 4 4 4 4 4 4
+4 3 3 3 3 3 4
+4 3 2 2 2 3 4
+4 3 2 1 2 3 4
+4 3 2 2 2 3 4
+4 3 3 3 3 3 4
+4 4 4 4 4 4 4
+
+    void printSquare(int n) {
+        // code here
+        for(int i=0; i<2*n-1; i++){
+            for(int j =0; j<2*n-1; j++){
+                int top = i;
+                int left =j;
+                int bottom = (2*n-2)-i;
+                int right =(2*n-2)-j;
+                cout<<(n-min(min(top, bottom), min(right,left)));
+                cout<<" ";
+            }
+            cout<<endl;
+        }
+    }
