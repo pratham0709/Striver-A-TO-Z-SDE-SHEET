69bee511bb90c9ec706919bcc64a9c7c776cd5fe
Prathamesh Jadhav <109234076+pratham0709@users.noreply.github.com>
2023-02-22 08:16:12 +0530

    Update ALL PATTERNS.cpp

--- ALL PATTERNS.cpp
+++ ALL PATTERNS.cpp
@@ -506,3 +506,25 @@ pattern 20:
             else spaces+=2;
         }
     }
+
+Pattern 21
+*****
+*   *
+*   *
+*   *
+*****
+
+void getStarPattern(int n) {
+    // Write your code here.
+          for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || j==0 || i==n-1 || j==n-1){
+                    cout<<"*";
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            cout<<endl;
+        }
+}
