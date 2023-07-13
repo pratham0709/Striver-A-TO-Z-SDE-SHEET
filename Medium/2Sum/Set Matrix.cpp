3d96fe8eb6c578b212f70877a37d1a3338ab0d04
Prathamesh Jadhav <109234076+pratham0709@users.noreply.github.com>
2023-07-13 17:41:55 +0530

    Add files via upload

---
+++ Medium/2Sum/Set Matrix.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+
+void setZeroes(vector<vector<int>> &matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    vector<int> row(n,0); // row array
+    vector<int> col(m,0); // col array
+
+    // Traverse the matrix:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                // mark ith index of row wih 1:
+                row[i] = 1;
+
+                // mark jth index of col wih 1:
+                col[j] = 1;
+            }
+        }
+    }
+
+    // Finally, mark all (i, j) as 0
+    // if row[i] or col[j] is marked with 1.
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+}
+};
