f0c967440c7070ef793c84aba7fadcc0b414b07b
Prathamesh Jadhav <109234076+pratham0709@users.noreply.github.com>
2023-07-27 19:06:13 +0530

    Update Move all zeroes to end of array.cpp

--- Array/Easy/Move all zeroes to end of array.cpp
+++ Array/Easy/Move all zeroes to end of array.cpp
@@ -37,3 +37,22 @@ int main()
     cout << '\n';
     return 0;
 }
+
+
+
+OR
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+      int n = nums.size();
+			int i=0;    
+      for(int j=0;j<n;j++){        
+          if(nums[j]!=0){            
+              swap(nums[i],nums[j]);            
+              i++;        
+            }    
+        }
+    }
+};
